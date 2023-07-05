f5b6825d43ecec7940380aac5df3efc8d3ab13f3
Marlen <02marlen95@gmail.com>
2023-07-04 18:55:34 -0600

    amarre

--- antesdelbody.js
+++
@@ -1,8 +0,0 @@
-window.onclick = function(){
-    //Me crea una serie de elements por cada tag encontrado
-    let encabezados = document.createElement("h2");
-    const text = document.createTextNode("¡Sal conmigo!");
-
-    encabezados.appendChild(text);
-    document.body.appendChild(encabezados);
-}
--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function moverPosicionRandom(elm){
     elm.style.position = 'absolute';
-    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px'; //para que no salga de la pantalla de lo ancho
-    elm.style.left = Math.random() * (window.innerWidth - elm.offsetinnerWidth) + 'px'; //para que no salga de la pantalla de lo alto
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetinnerWidth) + 'px';
 }
 
 let btnSi = document.getElementById("btn_si");
 let btnNo = document.getElementById("btn_no");
-let divModoSexo = document.getElementsByClassName("modo_hot")[0]; //solo hay uno -> valor 0
+let divModoSexo = document.getElementsByClassName("modo_hot")[0];
 
 btnNo.addEventListener('mouseenter', function(e) { moverPosicionRandom(e.target) });
 
 btnSi.addEventListener('click', function(e){
     alert('Sabía que dirías que SÍ. Casémonos ya y tengamos hijos. TE AMO!!! ❤');
 
-    divModoSexo.style.display = 'block'; //si aparece el elemento, hidden ahí sigue pero no se ve, none lo desaparece
+    divModoSexo.style.display = 'block';
     const cancion = new Audio('img\\modo_hot.mp3');
     cancion.play();
 });
